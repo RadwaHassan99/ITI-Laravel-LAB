050038372e3d3ae9db8cd57e0929000a5074dab4
Radwa99 <radwa.prof.9@gmail.com>
2023-03-25 14:27:35 +0200

    api pagination

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -18,6 +18,7 @@ class PostResource extends JsonResource
             'id'=>$this->id,
             'title'=>$this->title,
             'description'=>$this->description,
+            'comments_count' => $this->comments()->count(),
             'user'=>new UserResource($this->user),
         ];
     }
